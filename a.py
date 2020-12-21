40f6457e39e7d86eca03bbd1f3c2ef1456e66772
Sebyeok <tngnu02@gmail.com>
2020-12-21 13:13:10 +0000

    12

--- a.py
+++ a.py
@@ -1,2 +1,16 @@
-print('hello world!')
-print('Hi')
+import re
+
+name=input("File name?> ")
+
+handle = open(name)
+
+text=handle.read()
+
+digits=re.findall('[0-9]+',text)
+
+s=0
+
+for i in digits:
+  s = s + int(i)
+
+print(s)
